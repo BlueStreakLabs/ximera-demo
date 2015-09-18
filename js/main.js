777c66235152cea51a1b6c410e348f38799d9029
Kiran Bagul <bagulk@pun-bagulk-m.ds.ad.adp.com>
2015-09-19 00:57:33 +0530

    demo init

---
+++ js/main.js
@@ -0,0 +1,91 @@
+jQuery(function($) {
+    'use strict',
+
+   	new CBPFWTabs( document.getElementById( 'tabs-ui' ) );
+    //Scroll Menu
+
+    function menuToggle() {
+        var windowWidth = $(window).width();
+
+        if (windowWidth > 767) {
+            $(window).on('scroll', function() {
+                if ($(window).scrollTop() > 405) {
+                    $('.main-nav').addClass('fixed-menu animated slideInDown');
+                } else {
+                    $('.main-nav').removeClass('fixed-menu animated slideInDown');
+                }
+            });
+        } else {
+
+            $('.main-nav').addClass('fixed-menu animated slideInDown');
+
+        }
+    }
+
+    menuToggle();
+
+
+    // Carousel Auto Slide Off
+    //	$('#event-carousel, #twitter-feed, #sponsor-carousel ').carousel({
+    //		interval: false
+    //	});
+
+
+    // Contact form validation
+    var form = $('.contact-form');
+    form.submit(function() {
+        'use strict',
+        $this = $(this);
+        $.post($(this).attr('action'), function(data) {
+            $this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+        }, 'json');
+        return false;
+    });
+
+    $(window).resize(function() {
+        menuToggle();
+    });
+
+    $('.main-nav ul').onePageNav({
+        currentClass: 'active',
+        changeHash: false,
+        scrollSpeed: 900,
+        scrollOffset: 0,
+        scrollThreshold: 0.3,
+        filter: ':not(.no-scroll)'
+    });
+
+});
+
+
+// Google Map Customization
+(function() {
+
+    var map;
+
+    map = new GMaps({
+        el: '#gmap',
+        lat: 12.8483482,
+        lng: 77.674908,
+        scrollwheel: false,
+        zoom: 16,
+        zoomControl: false,
+        panControl: false,
+        streetViewControl: false,
+        mapTypeControl: false,
+        overviewMapControl: false,
+        clickable: false
+    });
+
+    var image = 'images/pin.png';
+    map.addMarker({
+        lat: 12.8483482,
+        lng: 77.674908,
+        icon: image,
+        animation: google.maps.Animation.DROP,
+        verticalAlign: 'bottom',
+        horizontalAlign: 'center',
+        backgroundColor: '#C34C39',
+    });
+
+}());
