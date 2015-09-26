a667dd407a8436fc3145212d65e26b8471ea9780
Kiran Bagul <bagulk@pun-bagulk-m.ds.ad.adp.com>
2015-09-26 20:57:15 +0530

    Removed schedule, font/color changes

--- js/main.js
+++ js/main.js
@@ -1,39 +1,38 @@
 jQuery(function($) {
-    'use strict',
+  'use strict',
 
-   	new CBPFWTabs( document.getElementById( 'tabs-ui' ) );
-    //Scroll Menu
+  // 	new CBPFWTabs( document.getElementById( 'tabs-ui' ) );
+  //Scroll Menu
 
-    function menuToggle() {
-        var windowWidth = $(window).width();
+  (function menuToggle() {
+    var windowWidth = $(window).width();
 
-        if (windowWidth > 767) {
-            $(window).on('scroll', function() {
-                if ($(window).scrollTop() > 405) {
-                    $('.main-nav').addClass('fixed-menu animated slideInDown');
-                } else {
-                    $('.main-nav').removeClass('fixed-menu animated slideInDown');
-                }
-            });
+    if (windowWidth > 767) {
+      $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 405) {
+          $('.main-nav').addClass('fixed-menu animated slideInDown');
         } else {
-            $('.main-nav').addClass('fixed-menu animated slideInDown');
+          $('.main-nav').removeClass(
+            'fixed-menu animated slideInDown');
         }
+      });
+    } else {
+      $('.main-nav').addClass('fixed-menu animated slideInDown');
     }
+  })();
 
+  $(window).resize(function() {
     menuToggle();
+  });
 
-    $(window).resize(function() {
-        menuToggle();
-    });
-
-    $('.main-nav ul').onePageNav({
-        currentClass: 'active',
-        changeHash: false,
-        scrollSpeed: 900,
-        scrollOffset: 0,
-        scrollThreshold: 0.3,
-        filter: ':not(.no-scroll)'
-    });
+  $('.main-nav ul').onePageNav({
+    currentClass: 'active',
+    changeHash: false,
+    scrollSpeed: 900,
+    scrollOffset: 0,
+    scrollThreshold: 0.3,
+    filter: ':not(.no-scroll)'
+  });
 
 });
 
@@ -41,31 +40,31 @@ jQuery(function($) {
 // Google Map Customization
 (function() {
 
-    var map;
+  var map;
 
-    map = new GMaps({
-        el: '#gmap',
-        lat: 12.8483482,
-        lng: 77.674908,
-        scrollwheel: false,
-        zoom: 16,
-        zoomControl: false,
-        panControl: false,
-        streetViewControl: false,
-        mapTypeControl: false,
-        overviewMapControl: false,
-        clickable: false
-    });
+  map = new GMaps({
+    el: '#gmap',
+    lat: 12.8483482,
+    lng: 77.674908,
+    scrollwheel: false,
+    zoom: 16,
+    zoomControl: false,
+    panControl: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+    overviewMapControl: false,
+    clickable: false
+  });
 
-    var image = 'images/pin.png';
-    map.addMarker({
-        lat: 12.8483482,
-        lng: 77.674908,
-        icon: image,
-        animation: google.maps.Animation.DROP,
-        verticalAlign: 'bottom',
-        horizontalAlign: 'center',
-        backgroundColor: '#C34C39',
-    });
+  var image = 'images/pin.png';
+  map.addMarker({
+    lat: 12.8483482,
+    lng: 77.674908,
+    icon: image,
+    animation: google.maps.Animation.DROP,
+    verticalAlign: 'bottom',
+    horizontalAlign: 'center',
+    backgroundColor: '#C34C39',
+  });
 
 }());
